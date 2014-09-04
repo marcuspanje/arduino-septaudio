ec77821053db425a0d6aa256151bfcbd50da1c41
marcuspanje <marcuspanje@gmail.com>
2014-09-03 20:25:07 -0400

    calibrate pwm

--- pwm/pwm.ino
+++ pwm/pwm.ino
@@ -1,14 +1,14 @@
 const int ledpin = 13;
 const int T = 400;
+const int offset = 65;
+const int T_real = 400 - offset;
+volatile int thigh = T_real/2;
 volatile int state = 0;
-const int sz  = 2;
-const int sz_1 = sz - 1;
-int vals[sz] = {50, 350};
-volatile int count = 0;
 volatile int a = 0;
-volatile int b = 0;
-volatile int c = 0;
-volatile int thigh = 100;
+volatile int count = 0;
+int vals[2] = {T_real/4, T_real/2};
+const int sz = sizeof(vals)/sizeof(int);
+const int sz_1 = sz - 1;
 
 void setup()
 {
@@ -20,43 +20,40 @@ void setup()
   TCCR1A = 0;
   TCCR1B = 0;
   TCNT1 = 0;
-  OCR1A = 200; // 200 gives int freq of 40kHz
+  //OCR1A = 0; // 200 gives int freq of 40kHz
   //TCCR1B |= (1 << WGM12); // WGM12 = 3 turn on CTC mode
   TCCR1B |= (1 << CS10); // CS10 = 0 no prescaling
   TIMSK1 |= (1 << OCIE1A); // OCIE1A = 1 interrupt mask for compare match register?
   sei();
   Serial.begin(9600);
-  Serial.println(WGM12);
-  Serial.println(CS10);
-  Serial.println(OCIE1A); 
-  
+  Serial.println(sz_1);
 }
 
 ISR(TIMER1_COMPA_vect) 
 {
   if (state) {  
-    OCR1A = TCNT1 + T - thigh;
-    //Serial.println(TCNT1);
-    //TCNT1 = 0; // reset timer
+    OCR1A = TCNT1 + T_real - thigh;
     PORTB &= B000000; //pin13 = LOW
     state = 0;
+   
   }
+  
   else {
-    OCR1A = TCNT1 + thigh;
-    a = OCR1A - TCNT1;
-    /*
-    if (count == sz - 1)
-      count = 0;
-    else
-      count ++;
-      */
-     // pin13 = HiGH
-    PORTB |= B100000;
+    thigh = vals[count];
+    
+    if (count == sz_1) count = 0;
+    else count++;
+    
+    OCR1A = TCNT1 + thigh;  
+    PORTB |= B100000; // pin13 = HiGH
     state = 1; 
   }
 }
 
 void loop()
 { 
-  Serial.println(a);
+  //change thigh
+ 
+  
+  
 }
