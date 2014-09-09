b4927ff1377e41133bb2f1a925227332d30b795e
marcuspanje <marcuspanje@gmail.com>
2014-09-09 15:12:40 -0400

    still configuring timer

--- pwm/pwm.ino
+++ pwm/pwm.ino
@@ -1,59 +1,72 @@
 const int ledpin = 13;
 const int T = 400;
-const int offset = 65;
-const int T_real = 400 - offset;
-volatile int thigh = T_real/2;
+
+volatile int start = 0;
+volatile int finish = 0;
+
+volatile int thigh = 280;
 volatile int state = 0;
 volatile int a = 0;
+
+volatile int finishT = 0;
 volatile int count = 0;
-int vals[2] = {T_real/4, T_real/2};
+int vals[2] = {100, 300};
 const int sz = sizeof(vals)/sizeof(int);
 const int sz_1 = sz - 1;
 
 void setup()
 {
+ Serial.begin(9600); 
   cli();
   DDRB = B100000;//set pin 13(6TH BIT) as output
   PORTB = B000000;//write 0
-  
   //set timer 1 registers
   TCCR1A = 0;
   TCCR1B = 0;
-  TCNT1 = 0;
-  //OCR1A = 0; // 200 gives int freq of 40kHz
+  OCR1A = 200; // 200 gives int freq of 40kHz
   //TCCR1B |= (1 << WGM12); // WGM12 = 3 turn on CTC mode
   TCCR1B |= (1 << CS10); // CS10 = 0 no prescaling
   TIMSK1 |= (1 << OCIE1A); // OCIE1A = 1 interrupt mask for compare match register?
+  TCNT1 = 0;
+  Serial.println(sizeof(TCNT1));
+  Serial.println(sizeof(OCR1A)); 
   sei();
-  Serial.begin(9600);
-  Serial.println(sz_1);
 }
 
 ISR(TIMER1_COMPA_vect) 
 {
-  if (state) {  
-    OCR1A = TCNT1 + T_real - thigh;
+  start = TCNT1;
+ 
+  if (state) {
+     
+    OCR1A += T - thigh;
     PORTB &= B000000; //pin13 = LOW
     state = 0;
-   
+    //finishT = 1;
+    // a = TCNT1;
   }
   
-  else {
-    thigh = vals[count];
-    
-    if (count == sz_1) count = 0;
-    else count++;
-    
-    OCR1A = TCNT1 + thigh;  
-    PORTB |= B100000; // pin13 = HiGH
+  else {  
+    //TCNT1 = 0;
+    OCR1A += thigh; 
+    PORTB |= B100000; // pin13 = HiGH     
     state = 1; 
   }
+  finish = TCNT1;
 }
 
 void loop()
-{ 
+{
+  Serial.println(finish-start);
+ // Serial.println(a);
   //change thigh
- 
-  
-  
+  /*
+  if (false) {
+ // if (finishT) {
+    thigh = 300;
+    if (count == sz_1) count = 0;
+    else count++;
+    finishT = 0;
+  }  
+    */    
 }
