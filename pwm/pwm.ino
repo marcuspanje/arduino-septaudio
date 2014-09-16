92737fd9e15584e34231dde522cee8c7d0559536
marcuspanje <marcuspanje@gmail.com>
2014-09-16 15:25:28 -0400

    new ISR with lotsof bugs

--- pwm/pwm.ino
+++ pwm/pwm.ino
@@ -7,11 +7,16 @@ volatile int state = 0;
 volatile unsigned int pwmcount = 0;
 volatile unsigned int pwmcount0 = 0;
 
+volatile int a = 99;
+volatile unsigned int count = 0;
+volatile int getnewT = 0;
+
 const int nvals = 2;
-int vals[nvals] = {200, 300}; //limit is 145
+int vals[nvals] = {200}; //limit is 145
 
 void setup()
 {
+  Serial.begin(9600);
   cli();
   DDRB = B100000; //set pin 13(6TH BIT) as output
   PORTB = B000000; //write 0
@@ -26,7 +31,7 @@ void setup()
   thigh = 200;
   tlow = 200;
   sei();
-  Serial.begin(9600);
+  
 }
 
 ISR(TIMER1_COMPA_vect) { 
@@ -34,37 +39,52 @@ ISR(TIMER1_COMPA_vect) {
   if (state) {     
     PORTB &= B000000; //pin13 = LOW
     OCR1A += tlow;
-    state = 0;
-    
-    /*
-    if (tlow < 100) {
-      while (TCNT < OCR1A) ;
+
+    if (tlow < 145) {
+      while (TCNT1 < OCR1A){}
+      getnewT = 1;
       PORTB |= B100000;
       OCR1A += thigh;
+
     }
+
     else {
       state = 0;
+      getnewT = 1;
     }
-    */
-
-    pwmcount++;
+    //pwmcount++;
   }
-  
+
   else {  
     PORTB |= B100000; // pin13 = HiGH     
     OCR1A += thigh;
-    state = 1; 
+    
+    if (thigh < 145) {
+      while (TCNT1 < OCR1A) {}
+      PORTB |=B000000;
+      OCR1A += tlow;
+      //pwmcount++;
+      getnewT = 1;
+    }
+    else state = 1; 
   }
-  
+
 }
 
 void loop()
 {
 
-  if (pwmcount0 != pwmcount)  {
-    pwmcount0 = pwmcount;
-    thigh = vals[pwmcount0 % nvals];
+  if (getnewT) {
+    count++;
+    thigh = vals[count % nvals];
     tlow = T - thigh;
+    Serial.println(thigh);
+    Serial.println(tlow);
+    getnewT = 0;
+    //a = pwmcount - pwmcount0;
+    //Serial.println(a);
   }
-   
+  
+
 }
+
