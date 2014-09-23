2f6d8e642af08893c60ecddfcadefe269d4e97c5
marcuspanje <marcuspanje@gmail.com>
2014-09-23 14:28:27 -0400

    faster adc

--- pwm_hardware/pwm_hardware.ino
+++ pwm_hardware/pwm_hardware.ino
@@ -1,17 +1,18 @@
 const int TOP = 400;
-float maxinput_v = 1.5;
-float maxinput_units = 1023*maxinput_v/5.0;
-float scale = TOP / maxinput_units;
-//int scale = 2;
+float input_TOP = 1024.0;
+float input_BOTTOM = 0;
+float scale = TOP/input_TOP;
 
 void setup()
 {
-  DDRC = B0;
-  
+  Serial.begin(9600);
+  cli();
   DDRB = B100; //set pin 13(6TH BIT) as output
+  
+//configure timers for pwm generation
   TCCR1A = 0;
   TCCR1B = 0;
- //WGM1(3:0) = 1111 fast pwm mode  ;  CS1(2:0) = 001 16Mhz clk not prescaled
+ //WGM1(3:0)=1111 fast pwm mode; CS1(2:0)=001 16Mhz clk not prescaled
  //COM1B(1:0) = 11 set oc1b on compare match
  //TCCR1A = B1011xx11 ; TCCR1B = B00011001
   TCCR1A = _BV(COM1B1) | _BV(COM1B0) | _BV(WGM11) | _BV(WGM10); 
@@ -19,19 +20,28 @@ void setup()
   OCR1A = TOP;
   OCR1B = 200;
   
-  Serial.begin(9600);
+  //configure ADC on analog input
+  DDRC = 0;//pin0 is input
+//MUX(3:0)=0000 ADC0(pin0) input; REFS(1:0)=01 Vref=Vcc(5V). 
+//ADPS(2:0)=101 ADCclk=16MHz/32=500kHz. Conversion takes 13 tcks of ADCclk 
+  ADMUX = 0;
+  ADCSRA = 0;
+  ADMUX = _BV(REFS0);
+  ADCSRA |= _BV(ADPS2) | _BV(ADPS0);
+  ADCSRA |= _BV(ADEN); //turn on adc
+  sei();
   
-  Serial.println('print');
-  Serial.println(maxinput_units);
-  Serial.println(scale);
+   
 }
 
 void loop()
 {
-  int a = analogRead(0);
-  //Serial.println(a);
-  OCR1B = (int) a*scale;
-  
-  
-  //change OCR1B
+  //read input
+  ADCSRA |= _BV(ADSC);//start ADC
+  int low = ADCL;
+  int high = ADCH; 
+  int adc = (high << 8) | low;
+  //Serial.println(adc); 
+  OCR1B = (int) adc*scale;
+ 
 }
--- test/test.ino
+++ test/test.ino
@@ -1,14 +1,17 @@
 int a = 1;
 void setup()
 {
-  Serial.begin(9600);
   
 }
 
 void loop()
 {
-  a++;
-  if (a%100 == 0)
-    Serial.println(a);
+  int a = 5;
+  int b = 6;
+  int c = a*b;
+  
+  float d = 5.0;
+  float e = 6.0;
+  float f = e*f;
   
 }
