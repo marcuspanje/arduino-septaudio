e2e94333c068bef95b6b6c490549cc4750793a30
Marcus Pan <marcuspanje@gmail.com>
2014-11-11 17:30:17 -0500

    port code to mega2560 board

--- test/test.ino
+++ test/test.ino
@@ -1,17 +1,16 @@
 
 void setup()
 {
- Serial.begin(9600);
-  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  //pinMode(11, OUTPUT);
+  DDRB = B100000;;
 }
 
 void loop()
 {
- /* digitalWrite(13, HIGH);
+  PORTB = B100000;
   delay(1000);
-  digitalWrite(13, LOW);
-  delay(1000);*/
-  delay(100);
-  Serial.println("1");
-  
+  PORTB = B0;
+  delay(1000);
 }
+
